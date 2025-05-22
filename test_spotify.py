a49f5b776beb868398fed15ece3530c85da646e0
zablon <zablonoigo51@gmail.com>
2025-05-22 10:42:57 +0300

    test top tracks by country

--- test_spotify.py
+++ test_spotify.py
@@ -1,6 +1,6 @@
 import pytest
 
-from spotify_pipeline import get_artist, get_token
+from spotify_pipeline import get_artist, get_token, get_top_tracks_by_countries
 
 
 @pytest.fixture
@@ -18,3 +18,11 @@ def test_get_artist_success(token):
     assert artist is not None
     assert "id" in artist
     assert artist["name"].lower() == "sauti sol"
+
+
+def test_get_top_tracks_by_country(token):
+    artist = get_artist(token, "Sauti Sol")
+    tracks = get_top_tracks_by_countries(token, artist["id"], countries=["KE"])
+    assert isinstance(tracks, list)
+    assert len(tracks) > 0
+    assert "track_name" in tracks[0]
