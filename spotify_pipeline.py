7bae7725ce2395ed7e0a3e6b8945cef6497f21e5
zablon <zablonoigo51@gmail.com>
2025-05-24 13:05:18 +0300

    add album method

--- spotify_pipeline.py
+++ spotify_pipeline.py
@@ -2,7 +2,7 @@ from typing import Dict, Iterator
 
 import dlt
 
-from main import get_artist, get_token
+from main import get_albums, get_artist, get_token
 
 
 @dlt.resource(name="artists")
@@ -17,3 +17,13 @@ def spotify_artist_data(artist_name: str) -> Iterator[Dict]:
             "followers": artist["followers"]["total"],
             "popularity": artist["popularity"],
         }
+
+
+@dlt.resource(name="albums")
+def spotify_album_data(artist_name: str) -> Iterator[Dict]:
+    token = get_token()
+    artist = get_artist(token, artist_name)
+    if artist:
+        albums = get_albums(token, artist["id"])
+        for album in albums:
+            yield album
