0f36b27ebfc849c057c53f6c302053287e5d5977
zablon <zablonoigo51@gmail.com>
2025-05-25 08:25:12 +0300

    track metrics by country

--- spotify_pipeline.py
+++ spotify_pipeline.py
@@ -2,7 +2,7 @@ from typing import Dict, Iterator
 
 import dlt
 
-from main import get_albums, get_artist, get_token
+from main import get_albums, get_artist, get_token, get_top_tracks_by_countries
 
 
 @dlt.resource(name="artists")
@@ -27,3 +27,15 @@ def spotify_album_data(artist_name: str) -> Iterator[Dict]:
         albums = get_albums(token, artist["id"])
         for album in albums:
             yield album
+
+
+@dlt.resource(name="track_metrics_by_country")
+def spotify_track_metrics_data(artist_name: str) -> Iterator[Dict]:
+    token = get_token()
+    artist = get_artist(token, artist_name)
+    if not artist:
+        return
+    artist_id = artist["id"]
+    tracks = get_top_tracks_by_countries(token, artist_id)
+    for track in tracks:
+        yield {**track}
